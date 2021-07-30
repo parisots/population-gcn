00f10c96225caafde1ea7baecfd14f501479ca3d
Tom Davies <tom.davies.96@hotmail.co.uk>
2021-07-30 16:12:56 +0100

    updated helper funcs

--- helper_funcs.py
+++ helper_funcs.py
@@ -1,6 +1,11 @@
 
 import sklearn
 import numpy as np
+import tensorflow.compat.v1 as tf
+from functools import partial
+from multiprocessing import Pool
+from main_ABIDE import train_fold_thread
+
 
 def compute_error_and_bias(y_true, y_pred, a):
     # https://github.com/matthklein/equalized_odds_under_perturbation/blob/master/equalized_odds.py
@@ -153,3 +158,70 @@ def get_auc_acc(y, pred, test, sex_data, population='all'):
       }
   )
   return scores_dict
+
+
+def process_scores(scores):
+  
+  metrics = ["auc", "acc", "acc_asd", "acc_neurotypical", "n", "n_asd", "n_neurotypical"]
+  score_keys = [
+    (metric, sex) for metric in metrics for sex in ["male", "female", "overall"]
+  ] + [("bias", "neurotypical"), ("bias", "asd")]
+
+  scores_dict = {key: [] for key in score_keys}
+
+  for score_tuple in scores:
+    pred = score_tuple[0]
+    test_ind = score_tuple[-1]
+    print('\nOverall')
+    overall_scores = get_auc_acc(y, pred, test_ind, sex_data)
+    for metric in metrics:
+      scores_dict[(metric, "overall")].append(overall_scores[metric])
+    scores_dict[("bias", "FP")].append(overall_scores["bias_0"])
+    scores_dict[("bias", "TP")].append(overall_scores["bias_1"])
+    
+    print('\nMale')
+    male_scores = get_auc_acc(y, pred, test_ind, sex_data, 'male')
+    for metric in metrics:
+      scores_dict[(metric, "male")].append(male_scores[metric])
+
+    print('\nFemale')
+    female_scores = get_auc_acc(y, pred, test_ind, sex_data, 'female')
+    for metric in metrics:
+      scores_dict[(metric, "female")].append(female_scores[metric])
+
+  columns = pd.MultiIndex.from_tuples(score_keys, names=["metric", "category"])
+  scores_df = pd.DataFrame(scores_dict, columns=columns)
+
+  return scores_df
+
+
+def run_cross_validation(strat_indices, graph, features, y, y_data, params, subject_IDs, skf, num_nodes, sex_data=None, stratify=False):
+
+    flags_dict = tf.flags.FLAGS._flags()    
+    keys_list = [keys for keys in flags_dict]    
+    for keys in keys_list:
+        tf.flags.FLAGS.__delattr__(keys)
+    tf.app.flags.DEFINE_string('f', '', 'kernel')
+
+    with Pool(processes=10) as process_pool:
+      scores = list(
+          process_pool.map(
+              partial(
+                  train_fold_thread, 
+                  graph_feat=graph, 
+                  features=features, 
+                  y=y, 
+                  y_data=y_data, 
+                  params=params, 
+                  subject_IDs=subject_IDs,
+                  sex_data=sex_data, 
+                  stratify=stratify,
+                  ),
+              # [(train_ind, test_ind, test_ind) for train_ind, test_ind in reversed(list(skf.split(np.zeros(num_nodes), np.squeeze(y))))]
+              strat_indices
+            )
+          )
+
+    # print(stratify)
+
+    return scores 
